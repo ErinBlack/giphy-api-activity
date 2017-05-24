7b8a0803d6a2ea52b5cd003755f7771c352dfc56
BluePawDev <jaruby@me.com>
2017-05-24 16:04:39 -0500

    Commit Hard mode

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,13 @@ console.log('js/jq sourced!');
 // on-click function
 $(document).on('click', '#cmdSearch', function(){
   console.log('cmdSearch clicked');
+
+if($('#txtSearch').val() === ''){
+  alert ('Enter a search key');
+}
+else {
+
+
   // concatenating URL string
   var searchURL = 'http://api.giphy.com/v1/gifs/search?q=';
   var userInput = $('#txtSearch').val();
@@ -15,14 +22,30 @@ $(document).on('click', '#cmdSearch', function(){
      url: searchURL,
      type: 'GET',
      success: searchResponse
+
    }); // end ajax
 
 function searchResponse(response) {
- console.log( 'back from search:', response.data[0].images.downsized.url );
+ // console.log( 'back from search:', response.data[0].images.downsized.url );
+ if(response.data.length ===0){
+   alert('You suck at searching AND spelling');
+ }else{
+ console.log('length of data', response.data.length);
+
  for (var i = 0; i < response.data.length; i++) {
-   $('#searchResults').append('<img src="' + response.data[i].images.downsized.url + '">');
+   var singleGif = $('<div class="gifCont"><img src="' + response.data[i].images.downsized.url + '">' + '<button id="deleteGif">remove</button></div>');
+   $('#searchResults').append(singleGif);
  }
 
+}
+ $('#txtSearch').val('');
+}
+
+//remove the gif with deleteGif button
+$('#searchResults').on('click', '#deleteGif', function(){
+  console.log('delete gif button working');
+  $(this).parent().remove();
+});
 }
 
 
